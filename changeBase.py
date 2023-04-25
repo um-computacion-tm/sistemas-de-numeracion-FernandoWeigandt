3815096b0bf16bd9687f9433ac48b53eb1c33ccf
FernandoWeigandt <f.weigandt@alumno.um.edu.ar>
2023-04-25 09:07:53 -0300

    Algo funcional

--- changeBase.py
+++ changeBase.py
@@ -25,15 +25,15 @@ def baseChanger_dec_oct(num):
 #Pasaje de Cualquier Base a Decimal
 
 def baseChanger_all_dec(num,base):
-    s=[]
-    dic_hex={'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
-    cont=0
-    for i in reversed(str(num)):
-        if i in dic_hex.keys():
-            i=dic_hex.get(i)
-        s.append(int(i)(base*cont))
-        cont+=1
-    return sum(s)
+    n = 0
+    num = str(num)
+    num = num[::-1]
+    for i in range(len(num)):
+        if num[i].isdigit():
+            n += int(num[i]) * base**i
+        else:
+            n += {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}.get(num[i]) * base**i
+    return n
 
 def baseChanger_bin_dec(num):
     return baseChanger_all_dec(num,2)
@@ -48,21 +48,10 @@ def baseChanger_oct_dec(num):
 #Pasaje de Binario a Octal y Hexadecimal
 
 def baseChanger_bin_to_oct_and_hex(num,base):
-    cadena_final= ''
     if base==8:
-        dic_bin = {"000": '0', "001": '1', "010": '2', "011": '3', "100": '4', "101": '5', "110": '6', "111": '7'}
-        salto=3
+        return baseChanger_dec_oct(baseChanger_bin_dec(num))
     elif base==16:
-        dic_bin = {
-            "0000": '0', "0001" : '1', "0010" : '2', "0011" : '3', "0100" : '4', "0101" : '5', "0110" : '6', "0111" : '7', "1000" : '8', "1001" : '9',
-              '1010' : 'A', '1011' : 'B', '1100' : 'C', '1101' : 'D', '1110' : 'E', '1111' : 'F'}
-        salto=4
-    for i in range (0,len(str(num)),salto):
-        num_3=str(num)[i:i+salto]
-        if num_3 in dic_bin.keys():
-            cadena_final+=str(dic_bin.get(num_3))
-
-    return cadena_final
+        return baseChanger_dec_hex(baseChanger_bin_dec(num))  
 
 def baseChanger_bin_oct(num):
     return baseChanger_bin_to_oct_and_hex(num,8)
@@ -74,31 +63,31 @@ def baseChanger_bin_hex(num):
 #Pasaje de Octal a Binario y Hexadecimal
 
 def baseChanger_oct_to_bin_and_hex(num,base):
-    cadena_final=''
     if base==2:
-        dic={'0' : "000", '1' : "001", '2' : "010", '3' : "011", '4' : "100", '5' : "101" , '6' : "110", '7' : "111"}
-        for i in str(num):
-            if i in dic.keys():
-                cadena_final+=str(dic.get(i))
+        return baseChanger_dec_bin(baseChanger_oct_dec(num))
     elif base==16:
-        cadena_final=hex(num)
-    return cadena_final
-
+        return baseChanger_dec_hex(baseChanger_oct_dec(num))
+    
 def baseChanger_oct_bin(num):
     return baseChanger_oct_to_bin_and_hex(num,2)
 
 def baseChanger_oct_hex(num):
     return baseChanger_oct_to_bin_and_hex(num,16)
 
-#ver oct to hex
 
 #Pasaje de Hexadecimal a Binario y Octal
 
 def baseChanger_hex_to_bin_and_oct(num,base):
-    cadena_final=''
+    if base==2:
+        return baseChanger_dec_bin(baseChanger_hex_dec(num))
+    elif base==8:
+        return baseChanger_dec_oct(baseChanger_hex_dec(num))
         
 def baseChanger_hex_bin(num):
     return baseChanger_hex_to_bin_and_oct(num,2)
 
 def baseChanger_hex_oct(num):
     return baseChanger_hex_to_bin_and_oct(num,8)
+
+
+
--- my_test.py
+++ my_test.py
@@ -2,12 +2,28 @@ import unittest
 from changeBase import *
 
 class TestBaseChanger(unittest.TestCase):
-    def test_1(self):
-        resultado = baseChanger_dec_bin(2)
-        self.assertEqual(resultado, 10)
 
+    def test_baseChanger_dec_all(self):
+        self.assertEqual(baseChanger_dec_all(10,2), '1010')
+        self.assertEqual(baseChanger_dec_all(15,16), 'F')
+        self.assertEqual(baseChanger_dec_all(123,8), '173')
 
-if __name__== '__main__':
+    def test_baseChanger_all_dec(self):
+        self.assertEqual(baseChanger_all_dec(1010,2), 10)
+        self.assertEqual(baseChanger_all_dec('FF',16), 255)
+        self.assertEqual(baseChanger_all_dec(173,8), 123)
+
+    def test_baseChanger_bin_to_oct_and_hex(self):
+        self.assertEqual(baseChanger_bin_to_oct_and_hex(1010,8), '12')
+        self.assertEqual(baseChanger_bin_to_oct_and_hex(1010,16), 'A')
+
+    def test_baseChanger_oct_to_bin_and_hex(self):
+        self.assertEqual(baseChanger_oct_to_bin_and_hex(173,2), '1110111')
+        self.assertEqual(baseChanger_oct_to_bin_and_hex(173,16), '7B')
+
+    def test_baseChanger_hex_to_bin_and_oct(self):
+        self.assertEqual(baseChanger_hex_to_bin_and_oct('FF',2), '11111111')
+        self.assertEqual(baseChanger_hex_to_bin_and_oct('FF',8), '377')
+
+if __name__ == '__main__':
     unittest.main()
-
-
